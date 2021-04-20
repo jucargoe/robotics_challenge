9997217d21a6d73f61489f1b7995bb06efd21cfe
Juan Carlos Gómez <juan.gomez@iagt.es>
2021-04-20 21:21:19 +0200

    WIP - improved robot movement and started integration of pyorca module

--- package_files_backup/challenge_orca.py
+++ package_files_backup/challenge_orca.py
@@ -5,21 +5,59 @@ import math
 import rospy
 import tf
 import numpy as np
+# import pyorca
 from geometry_msgs.msg import Twist, PointStamped
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 
 class Orca():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.listener = tf.TransformListener()
+        self.robot_position_x = 0.0
+        self.robot_position_y = 0.0
         self.goal_x = np.Infinity
         self.goal_y = np.Infinity
+        self.angular = 0.0
+        self.linear = 0.0
+
         rospy.Subscriber("/ScanDownsampler/laser", LaserScan, self.scan)
         rospy.Subscriber("/RvizListener/coordinates", PointStamped, self.coordinates)
+        rospy.Subscriber('/odom', Odometry, self.callback)
+
+    def callback(self, odometry):
+        # print(odometry)
+        self.robot_position_x = odometry.pose.pose.position.x
+        self.robot_position_y = odometry.pose.pose.position.y
+        # print(str(self.robot_position_x), str(self.robot_position_y))
 
     def scan(self, laser_data):
+        # TODO
         print("scan")
-        #print(laser_data)
+        # position = (0., 0.)
+        # velocity = (self.linear, self.angular)
+        # radius = 1.
+        # max_speed = 5
+        # pref_velocity = 0
+        # robot = pyorca.Agent(position, velocity, radius, max_speed, pref_velocity)
+        # agents = []
+
+        # for angle, laser_range in enumerate(laser_data.ranges):
+        #     if laser_range != np.Infinity:
+        #         x = np.sin(angle) * laser_range
+        #         y = np.cos(angle) * laser_range
+        #         print("x: " + str(x) + ", y: " + str(y))
+        #         position = (x, y)
+        #         velocity = (0., 0.)
+        #         radius = 1.
+        #         max_speed = 0
+        #         pref_velocity = 0
+        #         agent = pyorca.Agent(position, velocity, radius, max_speed, pref_velocity)
+        #         agents.append(agent)
+        #         # print(laser_range)
+        
+        # vel, lines = pyorca.orca(robot, agents, 0, 0.)
+        # print("vel: " + str(vel) + ", lines: " + str(lines))
 
     def coordinates(self, coordinates):
         self.goal_x = coordinates.point.x
@@ -29,25 +67,27 @@ class Orca():
         #rospy.loginfo("Command")
         if self.goal_x != np.Infinity or self.goal_y != np.Infinity:
             #print("x = " + str(self.goal_x) + ", y = " + str(self.goal_y))
-            angular = 0.0
-            linear = 0.0
             distance = math.sqrt((self.goal_x) ** 2 + (self.goal_y) ** 2)
 
+            # FIXME
             if distance > 0.1:
-                angular = math.atan2(self.goal_y, self.goal_x) / 2.0
+                self.angular = math.atan2(self.goal_y, self.goal_x) / 2.0
+                if self.angular > 0.3:
+                    self.angular = 0.3
+                elif self.angular < -0.3:
+                    self.angular = -0.3
 
-                if angular < 0.1 and angular > -0.1:
-                    linear = (distance * 0.5) / 1.0
-                    
-                """if angular > 0.3:
-                    angular = 0.3
-                if angular < -0.3:
-                    angular = -0.3
-                if linear > 0.5:
-                    linear = 0.5"""
+                if self.angular < 0.1 and self.angular > -0.1:
+                    self.linear += 0.1 if self.linear == 0.0 else self.linear * 0.25
 
-            print("distance: " + str(distance) + ", angular: " + str(angular) + ", linear: " + str(linear))
-            self.publish(linear, angular)
+                    if self.linear > 0.5:
+                        self.linear = 0.5
+            else:
+                self.angular = 0.0
+                self.linear = 0.0
+
+            # print("distance: " + str(distance) + ", angular: " + str(self.angular) + ", linear: " + str(self.linear))
+            self.publish(self.linear, self.angular)
 
     def publish(self, lin_vel, ang_vel):
         move_cmd = Twist()
